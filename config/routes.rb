8ea78cc638e7d21316500845ea9d7915403afc83
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-24 20:16:25 +0000

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root "lists#index"
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
-
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: :destroy
 end
